25bbf291edcbfe7659c6aab9aa215368cb8d90fb
Anastasia <55353420+Kulbina@users.noreply.github.com>
2023-03-23 07:02:07 +0300

    Update ApmManagerApplication.java

--- src/main/java/com/example/apm_manager/ApmManagerApplication.java
+++ src/main/java/com/example/apm_manager/ApmManagerApplication.java
@@ -13,59 +13,6 @@ public class ApmManagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApmManagerApplication.class, args);
-        // clientCRUDTest();
-    }
-    public static void printAllClients(ClientService clientService){
-        for(Client client: clientService.findAllClients()){
-            System.out.println(client.toString());
-        }
-    }
-
-    public static void printAllProposals(ProposalService proposalService){
-        for(Proposal proposal : proposalService.findAllProposals()){
-            System.out.println(proposal.toString());
-        }
-    }
-
-    public  static void clientCRUDTest(){
-        Client client = new Client(
-                "sobaka1",
-                "sobaka",
-                "sobaka",
-                "sobaka",
-                "sobaka",
-                454654,
-                "cat",
-                "cat",
-                "cat",
-                46546,
-                476574
-        );
-
-        ClientService clientService = new ClientService();
-        clientService.saveClient(client);
-        clientService.findByFIO("sobaka","5555", "sobaka");
-//        System.out.println("SAVE");
-//        printAllClients(clientService);
-//
-//
-//        client.setFirstName("5555");
-//        clientService.updateClient(client);
-//        System.out.println("UPDATE");
-//        printAllClients(clientService);
-//
-//        //clientService.deleteClient(client);
-//        System.out.println("DEL");
-//        printAllClients(clientService);
-//
-//        ProposalService proposalService = new ProposalService();
-//        proposalService.saveProposal(new Proposal(
-//                "open",
-//                212,
-//                1111,
-//                2222,
-//                client
-//        ));
-//        printAllProposals(proposalService);
+      
     }
 }
